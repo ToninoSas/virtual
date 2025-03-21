e22d9d5bdc9f3e3dc3d577ef634b51ef13c3c692
Antonio <86931984+ToninoSas@users.noreply.github.com>
2025-03-21 12:20:48 +0000

    first commit

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ meteo = {'sole': 1.0, 'pioggia': 0.8, 'neve': 0.6}  # Fattore di influenza sul n
 # Funzione per simulare una partita
 def simula_partita(squadra_A, squadra_B, meteo_corrente):
     # Fattore casuale per rendere il risultato imprevedibile
-    fattore_casuale = random.uniform(0.8, 1.2)
+    fattore_casuale_A = random.uniform(0.5, 1.5)
+    fattore_casuale_B = random.uniform(0.5, 1.5)
     
     # Calcolo dei gol segnati da ciascuna squadra
-    gol_A = (squadra_A['attacco'] / squadra_B['difesa']) * random.uniform(0.8, 1.2) * meteo[meteo_corrente]
-    gol_B = (squadra_B['attacco'] / squadra_A['difesa']) * random.uniform(0.8, 1.2) * meteo[meteo_corrente]
+    gol_A = (squadra_A['attacco'] / squadra_B['difesa']) * fattore_casuale_A * meteo[meteo_corrente]
+    gol_B = (squadra_B['attacco'] / squadra_A['difesa']) * fattore_casuale_B * meteo[meteo_corrente]
     
     # Arrotondamento dei gol
-    gol_A = round(gol_A)
-    gol_B = round(gol_B)
+    gol_A = round(gol_A)# Arrotondiamo e assicuriamoci che non sia negativo
+    gol_B = round(gol_B) 
     
     return gol_A, gol_B
 
